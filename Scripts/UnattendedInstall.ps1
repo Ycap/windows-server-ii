dd8b40ec9330a91f0247ad5f83f74569d2e364b1
Yorben Caplier <yorben.caplier@student.hogent.be>
2022-11-01 15:48:49 +0100

    Unattended install

---
+++ Scripts/UnattendedInstall.ps1
@@ -0,0 +1,56 @@
+Write-Host "Unattended install DHCP/DNS..."
+
+vboxmanage unattended install WinServerDHCPDNS `
+--iso="D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso" `
+--user=Administrator `
+--password=21Admin22 `
+--image-index=1 `
+--install-additions `
+--locale=be_NL `
+--country=BE `
+--time-zone=Europe/Brussels `
+--post-install-command="shutdown /r" `
+--start-vm=gui
+
+Write-Host "Unattended install IIS..."
+
+vboxmanage unattended install WinServerIIS `
+--iso="D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso" `
+--user=Administrator `
+--password=21Admin22 `
+--image-index=1 `
+--install-additions `
+--locale=be_NL `
+--country=BE `
+--time-zone=Europe/Brussels `
+--post-install-command="shutdown /r" `
+--start-vm=gui
+
+Write-Host "Unattended install Exchange..."
+
+vboxmanage unattended install WinServerExchange `
+--iso="D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso" `
+--user=Administrator `
+--password=21Admin22 `
+--image-index=1 `
+--install-additions `
+--locale=be_NL `
+--country=BE `
+--time-zone=Europe/Brussels `
+--post-install-command="shutdown /r" `
+--start-vm=gui
+
+
+Write-Host "Unattended install Domain Controller..."
+
+vboxmanage unattended install WinServerDC `
+--iso="D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso" `
+--user=Administrator `
+--password=21Admin22 `
+--image-index=1 `
+--install-additions `
+--locale=be_NL `
+--country=BE `
+--time-zone=Europe/Brussels `
+--post-install-command="shutdown /r" `
+--start-vm=gui
