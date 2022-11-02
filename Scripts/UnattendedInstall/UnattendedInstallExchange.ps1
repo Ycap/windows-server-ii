9b99b6e651e6143eaab9bb29594f46b65982b203
Yorben Caplier <yorben.caplier@student.hogent.be>
2022-11-02 15:43:01 +0100

    Updated unattended install

---
+++ Scripts/UnattendedInstall/UnattendedInstallExchange.ps1
@@ -0,0 +1,13 @@
+Write-Host "Unattended install Exchange..."
+
+vboxmanage unattended install Exchange `
+--iso="D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso" `
+--user=Administrator `
+--password=22Admin23 `
+--image-index=1 `
+--install-additions `
+--locale=be_NL `
+--country=BE `
+--time-zone=Europe/Brussels `
+--post-install-command="shutdown /r" `
+--start-vm=gui
--- Scripts/WinServerVMAanmaken.ps1
+++ Scripts/WinServerVMAanmaken.ps1
@@ -1,43 +1,47 @@
 #Aanmaken Windows DHCP/DNS VM
 Write-Host "##########Creating DHCP/DNS Server###########"
-vboxmanage createvm --name WinServerDHCPDNS --ostype Windows2019_64 --register --groups=/WindowsServerII
+vboxmanage createvm --name DHCPDNS --ostype Windows2019_64 --register --groups=/WindowsServerII
 
 #Configuratie Windows DHCP/DNS VM
-vboxmanage modifyvm WinServerDHCPDNS --memory=2048 --cpus 2 --vram=128 --nic1 intnet 
-vboxmanage storagectl WinServerDHCPDNS --name "SATA Controller" --add sata --controller IntelAhci --bootable on 
+vboxmanage modifyvm DHCPDNS --memory=2048 --cpus 2 --vram=128 --nic1 intnet 
+vboxmanage storagectl DHCPDNS --name "SATA Controller" --add sata --controller IntelAhci --bootable on 
+vboxmanage createhd --filename "D:\VirtualBox VMs\WindowsServerII\DHCPDNS\DHCPDNS.vdi" --size 30000
+vboxmanage storageattach DHCPDNS --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "D:\VirtualBox VMs\WindowsServerII\DHCPDNS\DHCPDNS.vdi" 
 
-vboxmanage storageattach WinServerDHCPDNS --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium "D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso"
 
 #Aanmaken Windows IIS VM
 Write-Host "##########Creating IIS Server###########"
-vboxmanage createvm --name WinServerIIS --ostype Windows2019_64 --register --groups=/WindowsServerII
+vboxmanage createvm --name IIS --ostype Windows2019_64 --register --groups=/WindowsServerII
 
 #Configuratie Windows IIS VM
-vboxmanage modifyvm WinServerIIS --memory=2048 --cpus 2 --vram=128 --nic1 intnet  
-vboxmanage storagectl WinServerIIS --name "SATA Controller" --add sata --controller IntelAhci --bootable on 
-
-vboxmanage storageattach WinServerIIS --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium "D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso"
+vboxmanage modifyvm IIS --memory=2048 --cpus 2 --vram=128 --nic1 intnet  
+vboxmanage storagectl IIS --name "SATA Controller" --add sata --controller IntelAhci --bootable on 
+vboxmanage createhd --filename "D:\VirtualBox VMs\WindowsServerII\IIS\IIS.vdi" --size 20000
+vboxmanage storageattach IIS --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "D:\VirtualBox VMs\WindowsServerII\IIS\IIS.vdi"
 
 #Aanmaken Windows Exchange VM
 Write-Host "##########Creating Exchange Server###########"
-vboxmanage createvm --name WinServerExchange --ostype Windows2019_64 --register --groups=/WindowsServerII
+vboxmanage createvm --name Exchange --ostype Windows2019_64 --register --groups=/WindowsServerII
 
 #Configuratie Windows Exchange VM
-vboxmanage modifyvm WinServerExchange --memory=8000 --cpus 4 --vram=128 --nic1 intnet  
-vboxmanage storagectl WinServerExchange --name "SATA Controller" --add sata --controller IntelAhci --bootable on
-vboxmanage storagectl WinServerExchange --name "SATA Controller #2" --add sata --controller IntelAhci 
+vboxmanage modifyvm Exchange --memory=8000 --cpus 4 --vram=128 --nic1 intnet  
+vboxmanage storagectl Exchange --name "SATA Controller" --add sata --controller IntelAhci --bootable on
+vboxmanage storagectl Exchange --name "SATA Controller #2" --add sata --controller IntelAhci 
 
-vboxmanage storageattach WinServerExchange --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium "D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso"
-vboxmanage storageattach WinServerExchange --storagectl "SATA Controller #2" --port 0 --device 0 --type dvddrive --medium "D:\VirtualBox VMs\Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO"
+vboxmanage createhd --filename "D:\VirtualBox VMs\WindowsServerII\Exchange\Exchange.vdi" --size 40000
+vboxmanage storageattach Exchange --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "D:\VirtualBox VMs\WindowsServerII\Exchange\Exchange.vdi"
+vboxmanage storageattach Exchange --storagectl "SATA Controller #2" --port 0 --device 0 --type dvddrive --medium "D:\VirtualBox VMs\Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO"
+
 #Aanmaken Windows DC VM
 Write-Host "##########Creating Domain Controller###########"
-vboxmanage createvm --name WinServerDC --ostype Windows2019_64 --register --groups=/WindowsServerII
+vboxmanage createvm --name DC --ostype Windows2019_64 --register --groups=/WindowsServerII
 
 #Configuratie Windows DC VM
-vboxmanage modifyvm WinServerDC --memory=3000 --cpus 2 --vram=128 --nic1 intnet  --nic2 nat
-vboxmanage storagectl WinServerDC --name "SATA Controller" --add sata --controller IntelAhci --bootable on 
+vboxmanage modifyvm DC --memory=3000 --cpus 2 --vram=128 --nic1 intnet  --nic2 nat
+vboxmanage storagectl DC --name "SATA Controller" --add sata --controller IntelAhci --bootable on 
 
-vboxmanage storageattach WinServerDC --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium "D:\VirtualBox VMs\en_windows_server_2019_x64_dvd_4cb967d8.iso"
+vboxmanage createhd --filename "D:\VirtualBox VMs\WindowsServerII\DC\DC.vdi" --size 40000
+vboxmanage storageattach DC --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "D:\VirtualBox VMs\WindowsServerII\DC\DC.vdi"
 
 
 
